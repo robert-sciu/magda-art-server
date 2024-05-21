cc41605bd57ae8d7583dd8242ce4860c8486741e
robert-sciu <robert.sciu@gmail.com>
2024-05-21 07:57:57 +0200

    email update

--- controllers/emailsController.js
+++ controllers/emailsController.js
@@ -1,13 +1,13 @@
 const { transporter } = require("../config/nodemailer");
 
 async function sendEmail(req, res, next) {
-  const { name, email, subject, text } = req.query;
+  const { name, email, subject, message } = req.query;
   try {
     const info = await transporter.sendMail({
       from: `${name} <${email}>`, // sender address
       to: `robert.sciu@gmail.com`, // list of receivers
-      subject, // Subject line
-      text: `Sender email: ${email}\n${text}`, // plain text body
+      subject: `Subject: ${subject}`, // Subject line
+      text: `Sender email: ${email}\n${message}`, // plain text body
       // html: "<b>Hello world?</b>", // html body
     });
     res.json({ status: "success", data: `Message sent: ${info.messageId}` });
