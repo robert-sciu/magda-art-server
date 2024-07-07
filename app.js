c306a1f32a1aba9d091ba6ec3a34a9a59312dd40
robert-sciu <robert.sciu@gmail.com>
2024-07-07 14:27:53 +0200

    prepared for server

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
-    origin: process.env.DEV_ORIGIN,
+    origin: process.env.ORIGIN,
     credentials: true,
   })
 );
