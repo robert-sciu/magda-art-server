7072cecd45b1bf3390e8ec5f645e6816404afcec
robert-sciu <robert.sciu@gmail.com>
2024-07-08 14:41:52 +0200

    preparations for production

--- config/minio.js
+++ config/minio.js
@@ -2,8 +2,8 @@ const Minio = require("minio");
 
 const minioClient = new Minio.Client({
   endPoint: process.env.BUCKET_ENDPOINT,
-  port: 9000,
-  useSSL: false,
+  port: process.env.MINIO_PORT,
+  useSSL: process.env.MINIO_USE_SSL,
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
 });
