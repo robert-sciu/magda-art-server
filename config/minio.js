c3874a102187c58a62827c90b650822de6bcb79d
robert-sciu <robert.sciu@gmail.com>
2024-07-08 14:57:07 +0200

    ready for first deployment

--- config/minio.js
+++ config/minio.js
@@ -2,8 +2,8 @@ const Minio = require("minio");
 
 const minioClient = new Minio.Client({
   endPoint: process.env.BUCKET_ENDPOINT,
-  port: process.env.MINIO_PORT,
-  useSSL: process.env.MINIO_USE_SSL,
+  port: 443,
+  useSSL: true,
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
 });
