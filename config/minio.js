981cc8077735e6ec2c5dabb00ec7dacf8fbed903
robert-sciu <robert.sciu@gmail.com>
2024-07-11 19:48:35 +0200

    turned off minio client if on ec2

--- config/minio.js
+++ config/minio.js
@@ -1,11 +1,15 @@
 const Minio = require("minio");
 
-const minioClient = new Minio.Client({
-  endPoint: process.env.BUCKET_ENDPOINT_MINIO,
-  port: 9000,
-  useSSL: false,
-  accessKey: process.env.MINIO_ACCESS_KEY,
-  secretKey: process.env.MINIO_SECRET_KEY,
-});
+function minio() {
+  return new Minio.Client({
+    endPoint: process.env.BUCKET_ENDPOINT_MINIO,
+    port: 9000,
+    useSSL: false,
+    accessKey: process.env.MINIO_ACCESS_KEY,
+    secretKey: process.env.MINIO_SECRET_KEY,
+  });
+}
+
+const minioClient = process.env.DISK_STORAGE === "minio" ? minio() : null;
 
 module.exports = minioClient;
