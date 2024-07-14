d5bef0f389d71f9f6e3017b326616ee22658b795
robert-sciu <robert.sciu@gmail.com>
2024-07-14 16:51:57 +0200

    reenabled jwt

--- config/jwt.js
+++ config/jwt.js
@@ -1,21 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 function authenticateJWT(req, res, next) {
-  // const token = req.headers.authorization;
-  // if (!token) {
-  //   res.status(403).json({ status: "error", message: "No token provided" });
-  //   return;
-  // }
-  // jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-  //   if (err) {
-  //     res.status(403).json({ status: "error", message: "Invalid token" });
-  //     return;
-  //   }
-  //   req.user = user;
-  //   next();
-  // });
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(403).json({ status: "error", message: "No token provided" });
+    return;
+  }
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    if (err) {
+      res.status(403).json({ status: "error", message: "Invalid token" });
+      return;
+    }
+    req.user = user;
+    next();
+  });
 
-  next();
+  // next();
 }
 
 module.exports = { authenticateJWT };
